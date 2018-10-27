a767654f710df9a8227cf7d21e1a599606c60d97
Gaurav <gauravyadavabviiitm@gmail.com>
2018-10-27 16:27:43 +0530

    first commit

---
+++ Movie recommender based on KNN.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 27 15:48:39 2018
+
+@author: Gaurav
+"""
+
+import pandas as pd
+import numpy as np
+from scipy import spatial
+import operator
+
+r_cols = ['user_id', 'movie_id', 'rating']
+ratings = pd.read_csv(r'C:\Users\Gaurav\Documents\DataScience\DataScience-Python3\ml-100k\u.data', sep='\t', names=r_cols, usecols=range(3))
+ratings.head()
+
+
+
+movieProperties = ratings.groupby('movie_id').agg({'rating': [np.size, np.mean]})
+movieProperties.head()
+
+movieNumRatings = pd.DataFrame(movieProperties['rating']['size'])
+movieNormalizedNumRatings = movieNumRatings.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+movieNormalizedNumRatings.head()
+
+movieDict = {}
+with open(r'C:\Users\Gaurav\Documents\DataScience\DataScience-Python3\ml-100k\u.item') as f:
+    temp = ''
+    for line in f:
+        #line.decode("ISO-8859-1")
+        fields = line.rstrip('\n').split('|')
+        movieID = int(fields[0])
+        name = fields[1]
+        genres = fields[5:25]
+        genres = map(int, genres)
+        movieDict[movieID] = (name, np.array(list(genres)), movieNormalizedNumRatings.loc[movieID].get('size'), movieProperties.loc[movieID].rating.get('mean'))
+
+
+
+def ComputeDistance(a, b):
+    genresA = a[1]
+    genresB = b[1]
+    genreDistance = spatial.distance.cosine(genresA, genresB)
+    popularityA = a[2]
+    popularityB = b[2]
+    popularityDistance = abs(popularityA - popularityB)
+    return genreDistance + popularityDistance
+    
+
+
+
+def getNeighbors(movieID, K):
+    distances = []
+    for movie in movieDict:
+        if (movie != movieID):
+            dist = ComputeDistance(movieDict[movieID], movieDict[movie])
+            distances.append((movie, dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(K):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+K = 10
+avgRating = 0
+neighbors = getNeighbors(1, K)
+for neighbor in neighbors:
+    avgRating += movieDict[neighbor][3]
+    print (movieDict[neighbor][0] + " " + str(movieDict[neighbor][3]))
+    
+avgRating /= K
